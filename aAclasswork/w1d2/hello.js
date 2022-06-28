56662cc22c15157f1cab1838bf41de2fa641248a
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-28 19:35:06 -0400

    complete hello

---
+++ aAclasswork/w1d2/hello.js
@@ -0,0 +1,14 @@
+// Write a function `hello` that takes in a string and prints out "Hello, " followed
+// by the string. HINT: this function doesn't need to return anything, it should
+// just print using console.log
+//
+// Examples:
+// hello("child");    // prints "Hello, child"
+// hello("Anthony");  // prints "Hello, Anthony"
+//
+function hello(str) {
+    console.log("Hello, " + str +".")
+  }
+
+hello("child");    // prints "Hello, child"
+hello("Anthony");  // prints "Hello, Anthony"
