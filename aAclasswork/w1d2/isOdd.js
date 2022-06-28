2cc1c4ece0596fefed6be25520bc1298e7c74ab0
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-28 19:11:49 -0400

    is

---
+++ aAclasswork/w1d2/isOdd.js
@@ -0,0 +1,24 @@
+// Write a function `isOdd` that takes in a number as an argument and returns `true`
+// if the number is odd and returns `false` otherwise.
+// Write two versions of this function, using conditionals and without using conditionals
+//
+// Examples:
+// isOdd(2);    // => false
+// isOdd(5);    // => true
+// isOdd(-17);  // => true
+
+// function isOdd(num) {
+//     if (num % 2 === 0) {
+//         console.log(false);
+//     } else {
+//         console.log(true);
+//         }
+//     }
+  
+function isOdd(number) {
+    return (number % 2 !== 0);
+}
+console.log(isOdd(2))
+
+console.log(isOdd(2));    // => false
+console.log(isOdd(-17));  // => true
