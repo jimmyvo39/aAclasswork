6f7f7a23022b1a4d40158db8277c7005634f0206
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-28 14:41:50 -0400

    completed conditional muscle memory excersise.

---
+++ aAclasswork/w1d1/problem_set_2/Conditional_Muscle_Memory/11.js
@@ -0,0 +1,54 @@
+// practice typing conditionals
+
+var num = 11; // feel free to change the value of this variable
+if (num > 5) {
+  console.log('if');
+}
+var num =  11;
+if (num > 5) {
+  console.log("if");
+}
+
+var num = 11;
+if(num > 5) {
+  console.log("if")
+}
+
+var num = 11;
+if (num > 5) {
+  console.log("if")
+}
+
+var num = 11;
+if (num < 5) {
+  console.log("if")
+}
+
+var num = 11;
+if (num > 5) {
+  console.log("if")
+}
+
+var num = 11;
+if(num < 5) {
+  console.log("if")
+}
+
+var num = 5;
+if(num > 5) {
+  console.log("if")
+}
+
+var num = 5;
+if(num > 5) {
+  console.log("if")
+}
+
+var num = 5;
+if(num > 5) {
+  console.log("if")
+}
+var num = 11;
+if(num > 5) {
+  console.log("if")
+}
---
+++ aAclasswork/w1d1/problem_set_2/Conditional_Muscle_Memory/12.js
@@ -0,0 +1,76 @@
+var num = 5; // feel free to change the value of this variable
+if (num > 5) {
+  console.log('if');
+} else {
+  console.log('else');
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if(num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if(num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num < 5){
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num < 5) {
+  console.log("if");
+} else {
+  console.log('else');
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+var num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
