cbaf0c68371bb22f21f327c7a89c1f812a5ebce8
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-29 08:12:37 -0400

    complete functionMuscleMemory

---
+++ aAclasswork/w1d2/functionMuscleMemory.js
@@ -0,0 +1,76 @@
+function sayHello(name) {
+    var msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+  }
+  
+   console.log(sayHello('bootcamp prep')); // feel free to change the string being passed
+
+  function sayHello(name) {
+    var msg = "Hello, " + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"));//1
+  
+  function sayHellow(name) {
+    var msg = "Hello, " + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//2
+
+  function sayHello(name) {
+    var msg = "Hello," + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//3
+
+  function sayHello(name) {
+    var msg = "Hello, " + name + ". How are you?";
+  } return msg;
+
+  console.log(sayHello("bootcamp prep"))//4
+
+  function sayHello(name) {
+    var msg = "Hello, " + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))// 5
+
+  function sayHello(name) {
+    var msg = "Hello, " + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//6
+
+  function sayHello(name) {
+    var msg = "Hello," + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//7
+  
+  function sayHello(name) {
+    var msg = "Hello," + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//8
+
+  function sayHello(name) {
+    var msg + "Hello," + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//9
+
+  function sayHello(name) {
+    var msg = "Hello," + name + ". How are you?";
+    return msg;
+  }
+
+  console.log(sayHello("bootcamp prep"))//10
+  
