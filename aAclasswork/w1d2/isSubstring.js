311ba8c2b72361fed9d0807f00ebcc0f17746633
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-28 20:00:30 -0400

    isSubstring Variant

--- aAclasswork/w1d2/isSubstring.js
+++ aAclasswork/w1d2/isSubstring.js
@@ -8,13 +8,18 @@
 // isSubstring("Time to program", "time"); // => true
 // isSubstring("Jump for joy", "joys"); // => false
 //
+// function isSubstring(searchString, subString) {
+//     if (searchString.indexOf(subString) > -1) {
+//         return true;
+//     } else {
+//         return false
+//     }
+//   }
+
 function isSubstring(searchString, subString) {
-    if (searchString.indexOf(subString) > -1) {
-        return true;
-    } else {
-        return false
-    }
-  }
+    return (searchString.indexOf(subString) > -1)
+}
+  
 console.log(isSubstring("The cat went to the store", "he cat went")); // => true
 console.log(isSubstring("Time to program", "time")); // => true
 console.log(isSubstring("Jump for joy", "joys")); // => false
