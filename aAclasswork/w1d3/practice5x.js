5e26c015c77df05be7588277180c379d1e396c2d
jimmyv39 <j.vo718@gmail.com>
2022-06-29 17:33:53 -0400

    complete exercise

--- aAclasswork/w1d3/practice5x.js
+++ aAclasswork/w1d3/practice5x.js
@@ -6,12 +6,106 @@ function printForwards(start, end) {
   
   printForwards(4, 11); // feel free to change the numbers being passed
 
-  function printBackwards(start, end) {
+
+
+function printForwards(start, end) {
+    for (var counter = start; counter <= end; counter++) {
+        console.log(counter);
+    }
+}
+
+printForwards(4, 11);//1
+
+function printForwards(start, end) {
+    for (var counter = start; counter <= end; counter++) {
+        console.log(counter);
+    }
+}
+
+printForwards(4, 11);//2
+
+function printForwards(start, end) {
+    for (var counter = start; counter <= end; counter++) {
+        console.log(counter)
+    }
+}
+
+printForwards(4, 11);//3
+
+function printForwards(start, End) {
+    for (var counter = start; counter <= end; counter++) {
+        console.log(counter)
+    }
+}
+
+printForwards(4, 11);//4
+
+function printForwards(start, end) {
+    for (var counter = start; counter <= end; Counter++) {
+        console.log(counter)
+    }
+}
+
+printForwards(4, 11);//5
+
+//--------------------------------------------------------
+
+function printBackwards(start, end) {
+   var i = end;
+   while (i >= start) {
+     console.log(i);
+     i--;
+    }
+}
+  
+printBackwards(-5, 6); // feel free to change the numbers being passed 
+
+function printBackwards(start, end) {
+    var i =end;
+    while (i >= start) {
+        console.log(i);
+        i--;
+    }
+}
+
+printBackwards(-5, 6)//1
+
+function printBackwards(start, end) {
+    var i= end;
+    while (i >= start) {
+        console.log(i);
+        i--;
+    }
+}
+
+printBackwards(-5, 6)//2
+
+function printBackwards(start, end) {
     var i = end;
     while (i >= start) {
-      console.log(i);
-      i--;
+        console.log(i);
+        i--;
     }
-  }
-  
-  printBackwards(-5, 6); // feel free to change the numbers being passed 
+}
+
+printBackwards(-5, 6);//3
+
+function printBackwards(start, end) {
+    var i = end;
+    while (i >= start) {
+        console.log(i);
+        i--;
+    }
+}
+
+printBackwards(-5, 6);//4
+
+function printBackwards(start, end) {
+    var i = end;
+    while (i >= start) {
+        console.log(i);
+        i--;
+    }
+}
+
+printBackwards(-5, 6);//5
