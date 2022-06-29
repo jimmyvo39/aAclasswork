4d5b183ef2be87cf4c9200cf83f66c0b86357b54
jimmyv39 <jimmyvo@Jimmys-MacBook-Pro.local>
2022-06-28 20:19:19 -0400

    complete averageOfFour

---
+++ aAclasswork/w1d2/averageOfFour.js
@@ -0,0 +1,16 @@
+// Write a function `averageOfFour(num1, num2, num3, num4)` that takes in four
+// numbers. The function should return the average of all of the numbers.
+//
+// Examples:
+//
+// averageOfFour(10, 10, 15, 5); // => 10
+// averageOfFour(3, 10, 11, 4); // => 7
+// averageOfFour(1, 2, 3, 4); // => 2.5
+
+function averageOfFour(num1, num2, num3, num4) {
+	console.log((num1 + num2 + num3 + num4) / 4)
+}
+
+averageOfFour(10, 10, 15, 5); // => 10
+averageOfFour(3, 10, 11, 4); // => 7
+averageOfFour(1, 2, 3, 4); // => 2.5
