ec86a7c5c4daff7a352ec34b63e8663ce49bdee4
jimmyv39 <j.vo718@gmail.com>
2022-06-30 09:28:56 -0400

    remove redundant line

--- aAclasswork/resource/isPrime.js
+++ aAclasswork/resource/isPrime.js
@@ -1,7 +1,5 @@
 function isPrime(number) {
-    if (number < 2) {
-        return false;
-    }
+
     for (i = 2; i < number; i++) {
         if (number % i === 0) {
             return false;
@@ -16,12 +14,12 @@ console.log(isPrime(9)); // => false
 console.log(isPrime(2017)); // => true
 
 /*
-if number is less than 2 it is not prime - 1 is not a prime number. this part is undefined for 2
+starting at 2 since 1 is not prime, 2 produce a false condition end loop and returns true
 
 
 if any between 2 & number-1  can divide into number evenenly it is not a prime number 
 
-each number is tested with the loop( does not work for 2) when the if condition is met the loop ends return the false statement
+each number is tested with the loop( does not work for 2) when the if condition is met return the false statement
 
 when the loop end based on condition of the loop it moves on to the rest of the function returning the true statement and ending the function
 
