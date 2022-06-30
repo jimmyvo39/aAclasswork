77413073dddc0728f99d49015005173498c56d2f
jimmyv39 <j.vo718@gmail.com>
2022-06-30 13:21:35 -0400

    reference for lopping through elements of an array

---
+++ aAclasswork/resource/printArray.js
@@ -0,0 +1,15 @@
+function printArray(arr) {
+    for (var i = 0; i < arr.length; i += 1) {
+      var ele = arr[i];
+      console.log(ele)
+    }
+  }
+  
+  /*
+ for loop iterated through every index
+the condition is flexible for all array lengths
+the last index is always at array.length - 1 hense i < array.length
+each element will print
+
+
+*/
