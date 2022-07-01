2308c27df9b7534aac5b085573da1d90961cd798
jimmyv39 <j.vo718@gmail.com>
2022-07-01 09:25:20 -0400

    complete luckySevens

--- aAclasswork/w1d5/3luckySevens.js
+++ aAclasswork/w1d5/3luckySevens.js
@@ -5,3 +5,18 @@
 //
 // luckySevens(25); // => [ 7, 14, 21 ]
 // luckySevens(42); // => [ 7, 14, 21, 28, 35, 42 ]
+
+//push new array only if i % 7
+
+function luckySevens(max) {
+    let newArray = []
+    for (i = 7; i <= max; i += 1) { // start from 7 instead of zero because 0 % by any number is 0
+        if (i % 7 === 0) {           
+            newArray.push(i)
+        }
+    }
+    return newArray
+}
+
+console.log(luckySevens(25))
+console.log(luckySevens(42))
