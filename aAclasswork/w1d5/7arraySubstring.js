94d1ece43fa349d600034fe71236ba6d45a18e1b
jimmyv39 <j.vo718@gmail.com>
2022-07-01 12:37:32 -0400

    complete arraySubstring

--- aAclasswork/w1d5/7arraySubstring.js
+++ aAclasswork/w1d5/7arraySubstring.js
@@ -9,3 +9,21 @@
 // => [true, false, true, true]
 // arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre")
 // => [true, false, false, true]
+
+//make new array push true or false base on condition
+
+function arraySubstring(words, str) {
+    let newArray = []
+    for (i = 0; i < words.length; i += 1) {
+        if (words[i].indexOf(str) > -1) {
+            newArray.push(true)
+        }
+        else {
+            newArray.push(false)
+        }
+    }
+    return newArray
+}
+
+console.log(arraySubstring(["hello", "history", "helix", "hellos"],"hel"))
+console.log(arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre"))
