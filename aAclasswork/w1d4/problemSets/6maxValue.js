1a79e932763be4235a43eaf97360c89aec3f97a3
jimmyv39 <j.vo718@gmail.com>
2022-07-01 00:28:41 -0400

    complete maxValue

---
+++ aAclasswork/w1d4/problemSets/6maxValue.js
@@ -0,0 +1,22 @@
+// Write a function `maxValue(array)` that returns the largest value in `array`.
+// Assume `array` is an array of numbers.
+//
+// Examples:
+//
+// maxValue([12, 6, 43, 2]); // => 43
+// maxValue([]); // => null
+// maxValue([-4, -10, 0.43]); // => 0.43
+// push array[0] in new array. compare every other array. if it is larger up date the new array  
+function maxValue(array) {
+    let newArray = [null]
+    newArray.push(array[0])
+
+    for (i = 0; i < array.length; i += 1) {
+        if(newArray[0] < array[i]) {
+            newArray[0] = array[i]
+        }
+    }
+    return newArray[0]
+}
+
+console.log(maxValue([2,4,7]))
