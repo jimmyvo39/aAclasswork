89d1003809668e21a71b8fdb25354e46c0d30939
jimmyv39 <j.vo718@gmail.com>
2022-07-01 09:31:51 -0400

    completecopyMachine

--- aAclasswork/w1d5/4copyMachine.js
+++ aAclasswork/w1d5/4copyMachine.js
@@ -7,3 +7,18 @@
 // copyMachine('candy', 2); // => [ 'candy', 'candy' ]
 // copyMachine('bread', 4); // => [ 'bread', 'bread', 'bread', 'bread' ]
 // copyMachine(11, 6); // => [ 11, 11, 11, 11, 11, 11 ]
+
+// loop num times, push element to new array
+
+function copyMachine(element, num) {
+    let newArray = []
+    for (i = 0; i < num; i += 1) {
+        newArray.push(element)
+    }
+    return newArray
+}
+
+console.log(copyMachine('candy', 0)); // => []
+console.log(copyMachine('candy', 2)); // => [ 'candy', 'candy' ]
+console.log(copyMachine('bread', 4)); // => [ 'bread', 'bread', 'bread', 'bread' ]
+console.log(copyMachine(11, 6)); // => [ 11, 11, 11, 11, 11, 11 ]
