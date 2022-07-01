964ccede26e0190808e138abb193727a5b31fbd5
jimmyv39 <j.vo718@gmail.com>
2022-07-01 13:04:56 -0400

    complete evenCaps

--- aAclasswork/w1d5/8evenCaps.js
+++ aAclasswork/w1d5/8evenCaps.js
@@ -8,3 +8,23 @@
 // => "ToM GoT A SmAlL PiEcE Of pIe"
 // evenCaps("the book is in front of the table")
 // => "ThE BoOk iS In fRoNt oF ThE TaBlE"
+
+//split sentence. push toUpperCase if index is even, else push toLowerCase
+
+function evenCaps(str) {
+    let newArray =[]
+    let strArray = str.split("")
+
+    for (i = 0; i < str.length; i += 1) {
+        if (i % 2 === 0) {
+            newArray.push(strArray[i].toUpperCase())
+        }
+        else {
+            newArray.push(strArray[i].toLowerCase())
+        }
+    }
+
+    return newArray.join("")
+}
+
+console.log(evenCaps("the book is in front of the table"))
