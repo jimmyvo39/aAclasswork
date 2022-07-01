96205206b71cbdbab28eee1bd1624047fda91e95
jimmyv39 <j.vo718@gmail.com>
2022-07-01 12:25:20 -0400

    complete wordYeller

--- aAclasswork/w1d5/6wordYeller.js
+++ aAclasswork/w1d5/6wordYeller.js
@@ -11,3 +11,41 @@
 // Example 2
 // var words = "Go to the store and grab the following: milk, bread, run, and cake";
 // wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!"; // => true
+
+//split the sentence into words
+//loop through each word
+//split the words into character
+//check indexof last character agaignt array of punctuations
+//if it matches then break the loop and push the word to new array
+//if the last character is not a punctuation make a new word by concatonating
+
+//make checking for puntuations a helper function
+
+function hasPunctuation(word) {
+  
+    let punctuation = [".", ",", "!", "?", ":", ";"]
+    let char = word.split("")
+    
+    return punctuation.indexOf(char[(char.length -1)]) > -1 
+  }
+
+function wordYeller(sentence) {
+    let newArray = []
+    let sentenceArray = sentence.split(" ")
+    
+    for (i = 0; i < sentenceArray.length; i += 1) {
+        let word = sentenceArray[i]
+        let newWord = word.concat("!")
+        
+        if (hasPunctuation(word)) {
+            newArray.push(word)
+        }
+        else {
+            newArray.push(newWord)
+        }
+    }
+    return newArray.join(" ")
+}
+
+
+console.log(wordYeller("Stop it now! Please, wont you stop?"))
