6f3a6afa33f7e200748467528883777372286da8
jimmyv39 <j.vo718@gmail.com>
2022-07-01 08:57:29 -0400

    complete muscleMemory2

--- aAclasswork/w1d4/problemSets/muscleMemory2.js
+++ aAclasswork/w1d4/problemSets/muscleMemory2.js
@@ -7,3 +7,42 @@ function printBackwards(array) {
   
   var names = ['kurstie', 'mike', 'alex', 'meagan'];
   printBackwards(names);
+
+  function printBackwards (array) {
+    for ( var i = array.length -1; i >= 0; i -= 1) {
+      var ele = array[i]
+      console.log(ele)
+    }
+  }
+
+  var names = ["kurstie", "mike", "alex", "meagan"]
+  printBackwards(names)//1
+
+  function printBackwards(array) {
+    for ( var i =array.length -1; i >= 0; i -=1) {
+      var ele =array[i]
+      console.log(ele)
+    }
+  }
+
+  names = ["kurstie", "mike", "alex", "meagan"]
+printBackwards(names)//2
+
+function printBackwards(array) {
+  for (var i = array.length-1; i >= 0; i-= 1) {
+    var ele = array[i]
+    console.log(ele)
+  }
+}
+names = ["kurstie", "mike", "alex", "meagan"]
+printBackwards(names)//3
+
+function printBackwards(array) {
+  for (var i = array.length -1; i >= 0; i -= 1) {
+    var ele = array[i]
+    console.log(ele)
+  }
+}
+
+names = ["kurstie", "mike", "alex", "meagan"]
+printBackwards(names)//4
