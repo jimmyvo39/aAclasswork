933795467068addb7ab382703a2c447f6f19612c
jimmyv39 <j.vo718@gmail.com>
2022-07-01 09:49:57 -0400

    complete everyOtherWord

--- aAclasswork/w1d5/5everyOtherWord.js
+++ aAclasswork/w1d5/5everyOtherWord.js
@@ -5,3 +5,19 @@
 //
 // everyOtherWord('hello how are you doing on this lovely day?'); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
 // everyOtherWord('the weather is wonderful'); // => [ 'the', 'is' ]
+
+// loop with a 2 stepper. push to new array. 
+
+function everyOtherWord(sentence) {
+    let newArray = []
+    let sentenceArray = sentence.split(" ")
+
+    for (i = 0; i < sentenceArray.length; i += 2) {
+        newArray.push(sentenceArray[i])
+
+    }
+    return newArray
+}
+
+console.log(everyOtherWord('hello how are you doing on this lovely day?'))
+console.log(everyOtherWord('the weather is wonderful'))
