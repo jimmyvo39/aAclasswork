feaf4cb29949ba9c1db60c35a26c3e38f1698e31
jimmyv39 <j.vo718@gmail.com>
2022-07-05 20:55:40 -0400

    complete adults

--- aAclasswork/w2d2/w2d2_pset/problem_set/problems/6_adults.js
+++ aAclasswork/w2d2/w2d2_pset/problem_set/problems/6_adults.js
@@ -14,23 +14,21 @@ var ppl = [
 
 adults(ppl); // => [ 'John', 'Jane' ]
 ***********************************************************************/
-// loop through ages
+// loop through ppl array
+//
 // if 18 or older push name to array
 
-var ppl = [
-  {name: 'John', age: 20},
-  {name: 'Jim', age: 13},
-  {name: 'Jane', age: 18},
-  {name: 'Bob', age: 7}
-];
-
 function adults(people) {
-  for (var key in obj) {
-    if (obj.)
+  let newArray = []
+
+  for (i = 0; i < people.length; i += 1) {
+    if(people[i].age >= 18) {
+      let obj = people [i]
+      newArray.push(obj.name)
+    }
   }
-
-
-
+  
+  return newArray
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
