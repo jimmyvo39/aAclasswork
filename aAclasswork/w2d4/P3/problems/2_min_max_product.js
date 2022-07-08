8a1f551764a816d29d379a1aafac9f3368a0e770
jimmyv39 <j.vo718@gmail.com>
2022-07-08 11:32:58 -0400

    complete min max product

--- aAclasswork/w2d4/P3/problems/2_min_max_product.js
+++ aAclasswork/w2d4/P3/problems/2_min_max_product.js
@@ -9,19 +9,25 @@ minMaxProduct([6, 3, 7, 2]) => 14
 minMaxProduct([0, 1, -5, 3, 6]) => -30
 *******************************************************************************/
 
-function minMaxProduct(array){
-  let obj = {min: 0, max: 0}
+function minMaxProduct(array) {
+  let obj = {}
+  obj["min"] = array[0]
+  obj["max"] = array[0]
+    
+    for (i = 0; i <array.length; i++) {
+      let num = array[i]
   
-  for (i = 0; i <array.length; i++) {
-    let num = array[i]
-
-    if (num < obj.min) {
-      obj.min = num
+      if (num < obj.min) {
+        obj.min = num
+      }
+      if (num > obj.max) {
+        obj.max =num
+      }
+  
     }
-
+    return obj.min * obj.max
   }
 
-}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = minMaxProduct;
