ef47b901db5d7122a988381029b83b1bbb92bef8
jimmyv39 <j.vo718@gmail.com>
2022-07-08 16:38:05 -0400

    complete long word count

--- aAclasswork/w2d5/w2d5_pset/problem_set/problems/1_long_word_count.js
+++ aAclasswork/w2d5/w2d5_pset/problem_set/problems/1_long_word_count.js
@@ -11,9 +11,21 @@ longWordCount("two reallylong words inthisstring"); // => 2
 longWordCount("allwordword longwordword wordswordword"); // => 3
 longWordCount("seventy schfifty five"); // => 1
 ***********************************************************************/
+// split into array. loop array count words with more than 7 characters
 
 function longWordCount(sentence) {
+    let wordArray = sentence.split(" ")
+    let count = 0
 
+    for (i =0; i < wordArray.length; i++) {
+        let word = wordArray[i]
+
+        if (word.length > 7) {
+            count += 1
+        }
+    }
+
+    return count
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
