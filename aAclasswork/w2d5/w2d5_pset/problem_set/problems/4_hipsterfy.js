f5ff057df50b89e9e31753c91c9461a53258fe7d
jimmyv39 <j.vo718@gmail.com>
2022-07-08 17:47:54 -0400

    complete hipsterfy

--- aAclasswork/w2d5/w2d5_pset/problem_set/problems/4_hipsterfy.js
+++ aAclasswork/w2d5/w2d5_pset/problem_set/problems/4_hipsterfy.js
@@ -12,7 +12,36 @@ hipsterfy("runner anaconda"); // => "runnr anacond"
 hipsterfy("turtle cheeseburger fries"); // => "turtl cheeseburgr fris"
 ***********************************************************************/
 
+//split into array loop through each word
+//scan words backwards at the first vowel unshift to new word
+//push new words to new array
+
 function hipsterfy(sentence) {
+    let words = sentence.split(" ")
+    let newSentence = []
+    let vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    
+    for (i = 0; i < words.length; i++) {
+        let word = words[i]
+        let char = words[i].split("")
+        let newWord = []
+        
+        for (j = word.length -1; j >= 0; j--) { 
+
+            if (vowels.indexOf(char[j]) > -1) {
+                break
+            }
+
+            newWord.unshift(char[j])
+        }
+        for (k = j -1 ; k >= 0; k--) { 
+            newWord.unshift(char[k])
+        }
+
+        newSentence.push(newWord.join(""))
+    }
+
+    return newSentence.join(" ")
 
 }
 
