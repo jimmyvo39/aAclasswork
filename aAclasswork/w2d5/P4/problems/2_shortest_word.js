4c73ed5cf75e80b5d166273393366f1fe0139da7
jimmyv39 <j.vo718@gmail.com>
2022-07-09 22:03:18 -0400

    complete shortest word

--- aAclasswork/w2d5/P4/problems/2_shortest_word.js
+++ aAclasswork/w2d5/P4/problems/2_shortest_word.js
@@ -8,8 +8,22 @@ shortestWord('app academy is cool') => 'is'
 shortestWord('bootcamp prep') => 'prep'
 *******************************************************************************/
 
+//split into array 
+//set shortWord to first word
+//loop through each word comparing lengths
+
 function shortestWord(sentence) {
-  // your code here...
+  let words = sentence.split(" ")
+  let obj = {}
+  obj["short"] = words[0]
+
+  for (i = 1; i < words.length; i++) {
+    if (obj["short"].length > words[i].length) {
+      obj["short"] = words[i]
+    }
+  }
+
+  return obj["short"]
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
