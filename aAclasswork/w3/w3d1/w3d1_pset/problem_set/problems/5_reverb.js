fbf393f67c1b000e01ee86ae42e679f03a86a602
jimmyv39 <j.vo718@gmail.com>
2022-07-11 20:32:20 -0400

    complete reverb

--- aAclasswork/w3/w3d1/w3d1_pset/problem_set/problems/5_reverb.js
+++ aAclasswork/w3/w3d1/w3d1_pset/problem_set/problems/5_reverb.js
@@ -11,7 +11,16 @@ reverb('my'); // => 'my'
 ***********************************************************************/
 
 function reverb(word) {
+    let vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    let chars = word.split("")
 
+    for (i = chars.length - 1; i >= 0; i--) {
+        if (vowels.includes(chars[i])){
+            return word + word.slice(i)
+        }
+    }
+
+    return word
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
