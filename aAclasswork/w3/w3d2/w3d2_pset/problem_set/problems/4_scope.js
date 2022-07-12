a927d8fc8380f8644435e764d243e844db529f26
jimmyv39 <j.vo718@gmail.com>
2022-07-12 19:22:52 -0400

    complete scope 4

--- aAclasswork/w3/w3d2/w3d2_pset/problem_set/problems/4_scope.js
+++ aAclasswork/w3/w3d2/w3d2_pset/problem_set/problems/4_scope.js
@@ -3,19 +3,22 @@
 // with `node problems/4_scope.js`
 
 var bar = 5;
-console.log("1: " + bar); //1: What will bar be on this line?
+console.log("1: " + bar); //1: What will bar be on this line? // 1: 5
 
 function foo() {
   var bar = 10;
-  console.log("3: " + bar); //3: What will bar be on this line?
+  console.log("3: " + bar); //3: What will bar be on this line? // 3: 10
 
   var innerFoo = function() {
     bar = 15;
   }
 
-  console.log("4: " + bar); //4: What will bar be on this line?
+  console.log("4: " + bar); //4: What will bar be on this line? // 4: 10
 }
 
-console.log("2: " + bar); //2: What will bar be on this line?
+console.log("2: " + bar); //2: What will bar be on this line? // 2: 5
 foo();
-console.log("5: " + bar); //5: What will bar be on this line?
+console.log("5: " + bar); //5: What will bar be on this line? 5: 5
+
+// prediction correct key 1, 2, 5, call for global bar.
+// key 3 & 4 call for bar within foo but not inner foo
