ea7d66274ca99cf475345a2e61d6b7a238f69d7d
jimmyv39 <j.vo718@gmail.com>
2022-07-12 19:15:23 -0400

    complete scope 3

--- aAclasswork/w3/w3d2/w3d2_pset/problem_set/problems/3_scope.js
+++ aAclasswork/w3/w3d2/w3d2_pset/problem_set/problems/3_scope.js
@@ -8,4 +8,7 @@ for (var foo = 0; foo <= 5; foo += 1) {
   console.log(foo); //What will foo be on each iteration?
 }
 
-console.log(foo); //What will foo be on this line?
+console.log(foo); //What will foo be on this line? - Happy
+
+// got prediction wrong, foo is mutated by the for loop since it is not a function.
+// print from 0 to 6 (iteration to breaking the loop)
