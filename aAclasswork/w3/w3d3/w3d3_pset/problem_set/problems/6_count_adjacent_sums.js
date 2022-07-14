86abfe567ee02c1583588c912da0d5b0dee1c4d7
jimmyv39 <j.vo718@gmail.com>
2022-07-14 14:08:43 -0400

    complete count adjacent sum

--- aAclasswork/w3/w3d3/w3d3_pset/problem_set/problems/6_count_adjacent_sums.js
+++ aAclasswork/w3/w3d3/w3d3_pset/problem_set/problems/6_count_adjacent_sums.js
@@ -11,7 +11,14 @@ countAdjacentSums([6, 7, 11, 2, 5, 10, 3], 13) //=> 3
 ***********************************************************************/
 
 function countAdjacentSums(arr, n) {
+    let count = 0
 
+    for (i = 0; i < arr.length; i++)
+    if (arr[i] + arr[i + 1] === n) {
+        count++
+    }
+
+    return count
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
