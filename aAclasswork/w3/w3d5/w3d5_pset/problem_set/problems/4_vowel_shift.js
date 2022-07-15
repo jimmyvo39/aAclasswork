3c63e474f4eb07a96f52302105523ae2656752e2
jimmyv39 <j.vo718@gmail.com>
2022-07-15 17:51:52 -0400

    complete vowel shift

--- aAclasswork/w3/w3d5/w3d5_pset/problem_set/problems/4_vowel_shift.js
+++ aAclasswork/w3/w3d5/w3d5_pset/problem_set/problems/4_vowel_shift.js
@@ -13,7 +13,21 @@ vowelShift('on the hunt'); // => 'un thi hant'
 ***********************************************************************/
 
 function vowelShift(sentence) {
+    let chars = sentence.split("") 
+    let vowelShift = {a: "e", e: "i", i: "o", o: "u", u: "a"}
+    let newSentence = []
 
+    for (let i = 0; i < chars.length; i++) {
+        let char = chars[i]
+
+        if (vowelShift[char] !== undefined) {
+            newSentence.push(vowelShift[char])
+        } else {
+            newSentence.push(char)
+        }
+    }
+
+    return newSentence.join("")
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
