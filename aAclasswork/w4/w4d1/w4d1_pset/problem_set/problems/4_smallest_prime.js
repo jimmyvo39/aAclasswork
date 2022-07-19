8feaf274963f3491a414e6ec5b0cf470fde2fdd7
jimmyv39 <j.vo718@gmail.com>
2022-07-19 15:00:42 -0400

    complete smallest prime

--- aAclasswork/w4/w4d1/w4d1_pset/problem_set/problems/4_smallest_prime.js
+++ aAclasswork/w4/w4d1/w4d1_pset/problem_set/problems/4_smallest_prime.js
@@ -20,9 +20,52 @@ smallestPrime(arr3); // => null
 var arr4 = [4, 6, 8, 10];
 smallestPrime(arr4); // => null
 ***********************************************************************/
+// make a new array by looping through og array and pushing prime numbers
+// loop through prime array and updating lowest number
+
+function isPrime(num) {
+    if (num < 2) {
+        return false
+    }
+
+    for (let i = 0; i < num; i ++) {
+        if(num % i === 0) {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+function lowest(num) {
+    let low =num[0]
+
+    for (let i = 1; i < num; i++) {
+        if (num[i] < lowest) {
+            lowest = num [i]
+        }
+    }
+
+    return low
+}
 
 function smallestPrime(array) {
+    let primeArr =[] 
 
+    if (array.length < 1) {
+        return null
+    }
+
+    for ( let i = 0; i < array.length; i++) {
+        if (isPrime(array[i])) {
+            primeArr.push(array[i])
+        } else {
+            return null
+        }
+    }
+
+    return lowest(primeArr)
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
