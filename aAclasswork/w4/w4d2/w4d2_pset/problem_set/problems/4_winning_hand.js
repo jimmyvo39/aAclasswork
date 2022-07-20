f7cd87d90e9b3179cc5b90dc4413297220e5617c
WeiZeng68 <weilouzeng@gmail.com>
2022-07-19 20:12:57 -0400

    question 4

--- aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/4_winning_hand.js
+++ aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/4_winning_hand.js
@@ -17,7 +17,25 @@ winningHand('KJKJ', 'QQQQ'); // => 'DRAW'
 ***********************************************************************/
 
 function winningHand(hand1, hand2) {
+    let cards = {
+        J: 1, 
+        Q: 2,
+        K: 3,
+    }
+    let score1 = 0;
+    let score2 = 0;
 
+    for (let i = 0; i < hand1.length; i++) {
+        score1 += cards[hand1[i]];
+        score2 += cards[hand2[i]];
+    }
+    if (score1 > score2) {
+        return hand1;
+    } else if (score2 > score1) {
+        return hand2;
+    } else {
+        return 'DRAW'
+    }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
