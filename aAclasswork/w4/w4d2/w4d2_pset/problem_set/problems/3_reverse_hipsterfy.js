ef099bd263e6d7d251efcb903b1f17594cb0b453
Justin Kilburn <thatmoonman@gmail.com>
2022-07-19 20:03:00 -0400

    rev hips

--- aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/3_reverse_hipsterfy.js
+++ aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/3_reverse_hipsterfy.js
@@ -11,9 +11,33 @@ reverseHipsterfy("towel flicker banana"); // => 'twel flcker bnna'
 reverseHipsterfy("runner anaconda"); // => 'rnner ncnda'
 reverseHipsterfy("turtle cheeseburger fries"); // => 'trtle chsbrger fres'
 ***********************************************************************/
+function removeVow(word){
+    let vowels = 'aeiou'
+    let newWord =''
+    let test = 0
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        let char = word[i]
+        
+        if (!vowels.includes(char)) {
+            newWord = char + newWord
+        } else if (vowels.includes(char) && test === 0) {
+            newWord = char + newWord
+            test = 1
+        }
+    }
+    return newWord;
+
+}
 
 function reverseHipsterfy(sentence) {
+    let words = sentence.split(" ")
+    let newSentence = []
 
+    for (let i = 0; i < words.length; i++) {
+        newSentence.push(removeVow(words[i]))
+    }
+    return newSentence.join(" ")
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
