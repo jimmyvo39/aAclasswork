425211f9a4fba36c38d15c58abc1c548117a694e
jimmyv39 <j.vo718@gmail.com>
2022-07-19 20:22:43 -0400

    complete shift character

--- aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/5_shift_chars.js
+++ aAclasswork/w4/w4d2/w4d2_pset/problem_set/problems/5_shift_chars.js
@@ -17,9 +17,23 @@ shiftChars('apple', 2); //=> 'crrng'
 shiftChars('bootcamp', 3); //=> 'errwfdps'
 shiftChars('zebra', 5); //=> 'ejgwf'
 ***********************************************************************/
+var alphabet = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'
+]
 
 function shiftChars(word, num) {
+  let newWord = ""
 
+  for (i = 0; i < word.length; i++) {
+    let letter = word[i]
+    let index = alphabet.indexOf(letter)
+    let newIndex = (index + num) % 26
+
+    newWord += alphabet[newIndex]
+  }
+
+  return newWord
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
