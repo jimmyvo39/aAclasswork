525394c4a95c68472acb07d20d7cd8946eb3c4dc
jimmyv39 <j.vo718@gmail.com>
2022-07-20 08:39:30 -0400

    complete double letter count

---
+++ aAclasswork/aA_Open/doubleLetterCount.js
@@ -0,0 +1,14 @@
+function doubleLetterCount(string) {
+    let count = 0
+    
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i + 1]) {
+            count++
+        }
+    }
+    
+    return count
+}
+
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
