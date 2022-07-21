95292d915da4ba95e8eebf467fdde44048a22dd1
jimmyv39 <j.vo718@gmail.com>
2022-07-21 17:41:48 -0400

    complete prime factors

--- aAclasswork/w4/w4d3/P7/problems/1_prime_factors.js
+++ aAclasswork/w4/w4d3/P7/problems/1_prime_factors.js
@@ -10,9 +10,30 @@ primeFactors(24) => [ 2, 3 ]
 primeFactors(30) => [ 2, 3, 5 ]
 primeFactors(11) => [11]
 *******************************************************************************/
+function isPrime(num) {
+  if (num < 2) {
+    return false
+  }
+
+  for (let i = 2; i < num; i++){
+    if (num % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
 
 function primeFactors(n) {
-  // your code here...
+  let newArr = []
+
+  for (let i = 0; i <= n; i++) {
+    if (isPrime(i) && n % i === 0) {
+      newArr.push(i)
+    }
+  }
+
+  return newArr
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
