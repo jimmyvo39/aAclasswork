2c98ed1b14ccbcf21f1611d2e48e8a7878c5690c
jimmyv39 <j.vo718@gmail.com>
2022-07-20 21:09:19 -0400

    complete nickname

--- aAclasswork/w4/w4d3/w4d3_pset/problem_set/problems/5_nickname.js
+++ aAclasswork/w4/w4d3/w4d3_pset/problem_set/problems/5_nickname.js
@@ -12,6 +12,27 @@ nickname('bob'); // => 'BOB-BOB'
 ***********************************************************************/
 
 function nickname(name) {
+    let vowels = "aeiou"
+    let vowelCount = 0
+    let index = 0
+
+    for (let i = 0; i < name.length; i++) {
+        let char = name[i]
+      
+        if(vowels.includes(char)){
+            vowelCount++
+        }
+
+        if (vowelCount === 2) {
+            index = i
+            let shortName = name.slice(0,index + 1)
+            return `${shortName.toUpperCase()}-${shortName.toUpperCase()}`
+        } 
+    }
+    
+  return `${name.toUpperCase()}-${name.toUpperCase()}`
+  
+
 
 }
 
