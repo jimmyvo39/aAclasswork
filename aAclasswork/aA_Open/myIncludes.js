a9026c9362471751755f18efaef876133a9e52c6
jimmyv39 <j.vo718@gmail.com>
2022-07-24 22:56:48 -0400

    complete my includes

---
+++ aAclasswork/aA_Open/myIncludes.js
@@ -0,0 +1,20 @@
+function myIncludes(arr, ele) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ele) {
+            return true
+        }
+    }
+    return false
+}
+
+
+
+
+
+
+
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'c')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'a')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'z')); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
