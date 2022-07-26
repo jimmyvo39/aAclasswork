7a1b4519426ebafc264715205366e6c0b7f8dcad
jimmyv39 <j.vo718@gmail.com>
2022-07-25 21:07:11 -0400

    com[plete min value

---
+++ aAclasswork/aA_Open/minValue.js
@@ -0,0 +1,25 @@
+function minValue(nums) {
+    if (nums.length === 0) {
+        return null
+    }
+    
+    let min = nums[0]
+    
+    for (let i = 1; i < nums.length; i++){
+        if(min > nums[i]) {
+            min = nums[i]
+        }
+        
+    }
+    
+    return min
+}
+
+
+
+
+
+
+console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
+console.log(minValue([-2, -3, -7, 3 ])); // -7
+console.log(minValue([])); // null
