f0704b83b64ffba6a69f0272d275ee8dab4e39ba
jimmyv39 <j.vo718@gmail.com>
2022-07-25 20:47:01 -0400

    complete has double letter

--- aAclasswork/resource/bubbleSort.js
+++ aAclasswork/resource/bubbleSort.js
@@ -5,9 +5,13 @@ function bubbleSort(arr) {
         sorted = true;
 
         for (let i = 0; i < arr.length - 1; i++) {
-            [arr[i + 1], arr[i]] =  [arr[i], arr[i + 1]];
-            sorted = false
+            if (arr[i] > arr[i +1]) {
+                [arr[i + 1], arr[i]] =  [arr[i], arr[i + 1]];
+                sorted = false
+            }
         }
     }
     return arr
 }
+
+console.log(bubbleSort([7, 6, 9, 1 ,8,1, 5, 4, 3, 1, 2]))
