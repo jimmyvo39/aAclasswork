b37fb69a6a6c8cbc69ef994df1835a5f51c9cf00
jimmyv39 <j.vo718@gmail.com>
2022-07-27 16:12:12 -0400

    complete reverse string

---
+++ aAclasswork/aA_Open/reverseString.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    let newstr =""
+    for (let i = str.length -1; i >= 0; i--) {
+        newstr += str[i]
+        
+    }
+    
+    return newstr
+}
+
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
