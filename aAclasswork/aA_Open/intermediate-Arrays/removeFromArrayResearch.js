9aa9c3478aac17128850041a4fb43dd8c34f2594
jimmyv39 <j.vo718@gmail.com>
2022-07-28 12:35:14 -0400

    complete remove from array research

---
+++ aAclasswork/aA_Open/intermediate-Arrays/removeFromArrayResearch.js
@@ -0,0 +1,24 @@
+
+function removeFromArray(location, arr) {
+    if ( location === "FRONT") {
+        arr.shift()
+    } else if ( location === "BACK") {
+        arr.pop()
+    } else {
+        console.log("ERROR")
+    }
+    
+    return arr
+
+}
+
+testArray = [0,1,2,3,4]
+
+removeFromArray("FRONT", testArray)
+console.log(testArray) // [1,2,3,4]
+
+removeFromArray("BACK", testArray)
+console.log(testArray) // [1,2,3]
+
+removeFromArray("MIDDLE", 4, testArray) // "ERROR"
+console.log(testArray) // [1,2,3]
