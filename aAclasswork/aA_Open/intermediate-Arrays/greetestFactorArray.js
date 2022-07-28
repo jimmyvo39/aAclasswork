15f5642e55c8c936d41d09ef349822de5e06738f
jimmyv39 <j.vo718@gmail.com>
2022-07-28 01:00:31 -0400

    complete greatest factor array

---
+++ aAclasswork/aA_Open/intermediate-Arrays/greetestFactorArray.js
@@ -0,0 +1,25 @@
+function greatestFactor(num){
+    for (let i = num - 1; i > 1; i--){
+        if(num % i === 0) {
+            return i
+        }
+    }
+}
+
+function greatestFactorArray(array) {
+    let newArr = []
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0){
+        newArr.push(greatestFactor(array[i]))
+        } else {
+        newArr.push(array[i])
+        }
+    }
+    
+    
+    return newArr
+}
+
+console.log(greatestFactorArray([16, 7, 9, 14])); // [8, 7, 9, 7]
+console.log(greatestFactorArray([30, 3, 24, 21, 10])); // [15, 3, 12, 21, 5]
