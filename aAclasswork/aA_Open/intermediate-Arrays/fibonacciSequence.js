b66e24050fb56b187421fa225038ffe71c3a49ab
jimmyv39 <j.vo718@gmail.com>
2022-07-28 00:47:07 -0400

    complete fbonacci sequence

---
+++ aAclasswork/aA_Open/intermediate-Arrays/fibonacciSequence.js
@@ -0,0 +1,28 @@
+function fibonacciSequence(length) {
+    if (length === 0) {
+        return []
+    } else if (length === 1) {
+        return [1]
+    } 
+    
+    let seq = [1 , 1]
+    
+    while (seq.length < length) {
+        let last = seq[seq.length - 1]
+        let secondToLast = seq[seq.length - 2]
+        let next = last + secondToLast
+        
+        seq.push(next)
+    }
+    
+    return seq
+}
+
+
+
+console.log(fibonacciSequence(4));  // [ 1, 1, 2, 3 ]
+console.log(fibonacciSequence(5));  // [ 1, 1, 2, 3, 5 ]
+console.log(fibonacciSequence(8));  // [ 1, 1, 2, 3, 5, 8, 13, 21 ]
+console.log(fibonacciSequence(0));  // [ ]
+console.log(fibonacciSequence(1));  // [ 1 ]
+console.log(fibonacciSequence(2));  // [ 1, 1 ]
