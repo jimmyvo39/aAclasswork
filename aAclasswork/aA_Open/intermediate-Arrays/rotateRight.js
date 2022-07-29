655704c41fd942870abafb940db378deebfc3bd5
jimmyv39 <j.vo718@gmail.com>
2022-07-29 10:45:26 -0400

    complete rotate right

---
+++ aAclasswork/aA_Open/intermediate-Arrays/rotateRight.js
@@ -0,0 +1,20 @@
+function rotateRight(array,num) {
+    let copy = array.slice()
+    
+    for (let i = 0; i < num; i++) {
+        let ele = copy.pop()
+        
+        copy.unshift(ele)
+    }
+    
+    return copy
+}
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
+console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+console.log(rotateRight(animals, 3)); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+console.log(animals); // [ 'wombat', 'koala', 'opossum', 'kangaroo' ]
